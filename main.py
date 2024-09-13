568d0f0b994790e0fbf1391134a9a2c967ac404d
Maria4782 <mashasalt2000@icloud.com>
2024-09-14 01:46:32 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-name= 'Топинамбур' # 1st program
+name= 'Топинамбур'# 1st program
+print(len(name))
 print(name[0])
 print(name[-1])
+print(len(name[5:10]))
 print(name[5:10])
 print(name[::-1])
+print(len(name[1:10:2]))
 print(name[1:10:2])
